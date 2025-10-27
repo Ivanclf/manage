d93a7ef6078805818178bef87a99c5adca15a358
Ivanclf <23009201447@stu.xidian.edu.cn>
2025-10-27 10:45:36 +0800

    chore: 活动类的初始化
    - 接口文档有部分改动
    - 新增了活动类的实体类和新增DTO，但没有实现

---
+++ src/main/java/com/activity/manage/pojo/entity/Activity.java
@@ -0,0 +1,15 @@
+package com.activity.manage.pojo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Activity implements Serializable {
+}
